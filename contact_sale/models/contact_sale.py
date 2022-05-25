36082a9d25e8936054ccfd985cfc767668cfca06
DhruvS-ict <dhruv.shah@icreativetechnolabs.com>
2022-05-25 18:43:48 +0530

    [ADD]contact_sale :- further done task(visible buttons according status and get follow up numbers: which count how many times status changes).

--- contact_sale/models/contact_sale.py
+++ contact_sale/models/contact_sale.py
@@ -12,9 +12,9 @@ class ContactSale(models.Model):
     _rec_name = 'contact_sale_name'
 
     contact_sale_name = fields.Char(string="Name", readonly=1)
-    contact_id = fields.Many2one('res.partner', string="Contact", tracking=True)
-    sale_order_id = fields.Many2one('sale.order', string="Sale Order", tracking=True)
-    sales_person = fields.Many2one('res.users', related="sale_order_id.user_id", readonly=1)
+    contact_id = fields.Many2one('res.partner', string="Contact")
+    sale_order_id = fields.Many2one('sale.order', string="Sale Order")
+    sales_person = fields.Many2one('res.users', related="sale_order_id.user_id", store=True, readonly=1)
     follow_ups_no = fields.Integer(string="Followups Number")
     contact_sale_history_lines_ids = fields.One2many('contact.sale.history', 'contact_sale_id',
                                                      string="Contact Sale History Lines")
@@ -23,7 +23,7 @@ class ContactSale(models.Model):
 
     @api.model
     def create(self, vals):
-        print("Contact Sale Token Generates in process................", vals)
+        print("...........................Contact Sale Token Generates in process................", vals)
         vals['contact_sale_name'] = self.env['ir.sequence'].next_by_code("contact.sale")
         return super(ContactSale, self).create(vals)
 
@@ -31,21 +31,35 @@ class ContactSale(models.Model):
         print("DDD")
         for rec in self:
             rec.status = 'draft'
+        self.message_post(body=(('%s has been changed') % (self.contact_id.name),
+                                ('%s has been changed') % (self.sale_order_id.name)))
+        self.follow_ups_no += 1
 
     def contact_sale_in_progress(self):
         print("III")
         for rec in self:
             rec.status = 'in progress'
+        self.message_post(body=(('%s has been changed') % (self.contact_id.name),
+                                ('%s has been changed') % (self.sale_order_id.name)))
+        self.follow_ups_no += 1
 
     def contact_sale_done(self):
         print("DoDoDo")
         for rec in self:
             rec.status = 'done'
+        self.message_post(body=(('%s has been changed') % (self.contact_id.name),
+                                ('%s has been changed') % (self.sale_order_id.name)))
+        self.follow_ups_no += 1
 
     def contact_sale_cancel(self):
         print("CCC")
         for rec in self:
             rec.status = 'cancel'
+        self.message_post(body=(('%s has been changed') % (self.contact_id.name),
+                                ('%s has been changed') % (self.sale_order_id.name)))
+        self.follow_ups_no += 1
+
+
 
 
 class ContactSaleHistory(models.Model):
@@ -61,9 +75,15 @@ class ContactSaleHistory(models.Model):
 
 
 
-# • Contact (M2O Res Partner)
-# • Sale Order (Allow to select Sale Order which has above selected contact)
-# • Salesperson (Automatically set based on Sale Order)
-# • Status (Draft, in progress, Done, Cancel)
-# • No. of follow ups
-# • Contact Sale History Lines (O2M of Contact Sale History)
+# Define fields
+# partner_id = fields.Many2one('res.partner',  'Partner') # This standard field.
+# partner_id_new = fields.Many2one('res.partner',  'Partner New') # This custom field.
+# partner_id_old = fields.Many2one('res.partner',  'Partner' Old) # This custom field.
+#
+# Define onchange method
+# @api.onchange('partner_id')
+# def onchange_partner_id(self):
+#     OldPartner = self.partner_id_new
+#     self.partner_id_new = self.partner_id
+#     self.partner_id_old = OldPartner
+# you can get old partner on partner_id_old fields.)
