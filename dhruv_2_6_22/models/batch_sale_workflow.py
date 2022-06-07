571c7a212a6b45ba5defd1b2c6645c52c64872bb
DhruvS-ict <dhruv.shah@icreativetechnolabs.com>
2022-06-07 18:05:27 +0530

    [ADD]dhruv_2_6_22 :- Training period exam(completed all tasks of Backend & Frontend).

--- dhruv_2_6_22/models/batch_sale_workflow.py
+++ dhruv_2_6_22/models/batch_sale_workflow.py
@@ -15,12 +15,13 @@ class BatchSaleWorkFlow(models.Model):
     batch_responsible_id = fields.Many2one('res.users', string="Responsible")
     """"""
     batch_operation_type = fields.Selection([('confirm', 'Confirm'), ('cancel', 'Cancel'),
-                                             ('merge', 'Merge')], string="Operation Type")
+                                             ('merge', 'Merge')], string="Operation Type", default='confirm',
+                                            required=True)
     """"""
     batch_customer_id = fields.Many2one('res.partner', string="Customer")
     """"""
     batch_status = fields.Selection([('draft', 'Draft'), ('done', 'Done'),
-                                     ('cancel', 'Cancel')], string="status")
+                                     ('cancel', 'Cancel')], string="status", default='draft')
     """"""
     batch_sale_order_ids = fields.Many2many('sale.order', 'batch_sale_workflow_sale_order',
                                             'batch_sale_workflow_id', 'sale_order_id', string="Sale Order")
@@ -36,7 +37,18 @@ class BatchSaleWorkFlow(models.Model):
         print("PPP")
         for rec in self:
             rec.batch_status = 'done'
-        self.batch_sale_order_ids.date_order = self.batch_operation_date
+        self.batch_sale_order_ids.write({
+            'date_order': self.batch_operation_date
+        })
+        # sale_env = self.env['sale.order']
+        if self.batch_operation_type in 'confirm':
+            self.batch_sale_order_ids.write({'state': 'sale'})
+        elif self.batch_operation_type in 'cancel':
+            self.batch_sale_order_ids.write({'state': 'cancel'})
+        elif self.batch_operation_type in 'merge':
+            self.batch_sale_order_ids.write({'state': 'cancel'})
+            self.env['sale.order'].create({'partner_id': self.batch_customer_id.id,
+                                           'order_line': self.batch_sale_order_ids.order_line})
 
     def cancel(self):
         print("CCC")
@@ -48,16 +60,20 @@ class BatchSaleWorkFlow(models.Model):
         for rec in self:
             rec.batch_status = 'draft'
 
-    def done(self):
-        print("DDD")
-        for rec in self:
-            rec.batch_status = 'done'
-
-    @api.onchange('batch_operation_type')
+    @api.onchange('batch_operation_type', 'batch_responsible_id')
     def confirm_state(self):
         """This is onchange api model."""
-        if self.batch_operation_type in 'confirm':
-            domain = [('state', '=', ['|', (['draft', 'sent'])]),
-                      ('user_id', '=', self.batch_responsible_id.id)]
-            return {'domain': {'batch_sale_order_ids': domain}}
-        
+        if self.batch_operation_type:
+            if self.batch_operation_type in 'confirm':
+                domain = [('state', '=', ['draft', 'sent']),
+                          ('user_id', '=', self.batch_responsible_id.id)]
+                return {'domain': {'batch_sale_order_ids': domain}}
+            elif self.batch_operation_type in 'cancel':
+                domain = [('state', '=', ['draft', 'sent', 'sale']),
+                          ('user_id', '=', self.batch_responsible_id.id)]
+                return {'domain': {'batch_sale_order_ids': domain}}
+            elif self.batch_operation_type in 'merge':
+                domain = [('state', '=', ['draft', 'sent']),
+                          ('user_id', '=', self.batch_responsible_id.id),
+                          ('partner_id', '=', self.batch_customer_id.id)]
+                return {'domain': {'batch_sale_order_ids': domain}}
